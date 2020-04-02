0ac9732e88dc96268a5259f49c297c63a67ab4a7
Assemgul <kassemguli@gmail.com>
2020-04-03 01:40:20 +0600

    added fonts

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require("fs");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -9,7 +10,12 @@ const PATHS = {
     src: path.join(__dirname, '../src'),
     dist: path.join(__dirname, '../dist'),
     assets: 'assets/'
-}
+};
+
+const PAGES_DIR = PATHS.src;
+const PAGES = fs
+    .readdirSync(PAGES_DIR)
+    .filter(fileName => fileName.endsWith(".html"));
 
 module.exports = {
 
@@ -54,6 +60,13 @@ module.exports = {
                     loader: {
                         scss: "vue-style-loader!css-loader!sass-loader"
                     }
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
                 }
             },
             {
@@ -106,14 +119,18 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: `${PATHS.assets}css/[name].[hash].css`,
         }),
-        new HtmlWebpackPlugin({
-            template: `${PATHS.src}/index.html`,
-            filename: './index.html',
-            inject: true
-        }),
+
         new CopyWebpackPlugin([
             { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+            { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
             { from: `${PATHS.src}/static`, to: '', },
-        ])
+        ]),
+        ...PAGES.map(
+            page =>
+            new HtmlWebpackPlugin({
+                template: `${PAGES_DIR}/${page}`,
+                filename: `./${page}`
+            })
+        )
     ]
 };
